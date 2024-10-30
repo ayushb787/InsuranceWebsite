28417f7693b3a4a5b7c7ebe639ce19daadf6366a
21BAI1124 <ayushomprakash.bhandari2021@vitsutdent.ac.in>
2024-10-30 16:27:13 +0530

    Services section added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
 import { FeaturesComponent } from './features/features.component';
+import { ServicesComponent } from './services/services.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent, HeroComponent, FeaturesComponent],
+  imports: [RouterOutlet,NavbarComponent, HeroComponent, FeaturesComponent, ServicesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
